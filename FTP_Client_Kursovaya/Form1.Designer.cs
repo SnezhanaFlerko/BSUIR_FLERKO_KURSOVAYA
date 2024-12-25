d21e3b49a53d26deb77c363a306f4329b5655740
Snezhana <snezhanaflerko@gmail.com>
2024-12-25 11:48:26 +0300

    initial commit

---
+++ FTP_Client_Kursovaya/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace FTP_Client_Kursovaya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.colName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Адрес:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(424, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Пароль:";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(63, 9);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(180, 20);
+            this.txtHost.TabIndex = 3;
+            this.txtHost.Text = "ftp://";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(300, 11);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(118, 20);
+            this.txtUsername.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(481, 9);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(118, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(619, 7);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(99, 24);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Соединение";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colName,
+            this.colSize,
+            this.colDate});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(2, 73);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(726, 402);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // colName
+            // 
+            this.colName.Text = "Имя";
+            this.colName.Width = 364;
+            // 
+            // colSize
+            // 
+            this.colSize.Text = "Размер";
+            this.colSize.Width = 176;
+            // 
+            // colDate
+            // 
+            this.colDate.Text = "Дата создания";
+            this.colDate.Width = 182;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "DIR.png");
+            this.imageList1.Images.SetKeyName(1, "FILE.png");
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSeparator1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripSeparator2,
+            this.toolStripButton5,
+            this.toolStripSeparator3,
+            this.toolStripButton6});
+            this.toolStrip1.Location = new System.Drawing.Point(15, 45);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(168, 25);
+            this.toolStrip1.TabIndex = 8;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.Visible = false;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "Back";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "Download File";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "Upload File";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "Delete";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton5.Text = "Create Folder";
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton6.Text = "New Connection";
+            this.toolStripButton6.Click += new System.EventHandler(this.toolStripButton6_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(249, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Логин:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 477);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "FTP_Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader colName;
+        private System.Windows.Forms.ColumnHeader colSize;
+        private System.Windows.Forms.ColumnHeader colDate;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton toolStripButton6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FTP_Client_Kursovaya/Form1.cs
@@ -0,0 +1,268 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FTP_Client_Kursovaya
+{
+    public partial class Form1 : Form
+    {
+
+        string Host = "", UserName, Password;
+        string first;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.MouseDoubleClick += new MouseEventHandler(listView1_MouseDoubleClick);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (Host == first)
+            {
+                MessageBox.Show("Вы уже в корне сервера");
+                return;
+            }
+            Client client = new Client(Host, UserName, Password);
+            string NewHost = new string(Host.ToCharArray().Reverse().ToArray());
+            int ind = NewHost.IndexOf("/");
+            NewHost = NewHost.Remove(0, ind+1);
+            Host = new string(NewHost.ToCharArray().Reverse().ToArray());
+            btnConnect_Click(null, null);
+
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+            string file = openFileDialog1.FileName;
+            string file2 = new string(file.ToCharArray().Reverse().ToArray());
+            int ind = file2.IndexOf("\\");
+            file2 = file2.Substring(0, ind);
+            file2 = new string(file2.ToCharArray().Reverse().ToArray());
+
+            Client client = new Client(Host, UserName, Password);
+            try
+            {
+                client.UploadFile(file, file2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            btnConnect_Click(null, null);
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count <= 0)
+            {
+                MessageBox.Show("Выделите файл");
+                return;
+            }
+            string ftype = listView1.SelectedItems[0].SubItems[1].Text.Trim();
+            if (ftype != "<DIR>")
+            {
+                try
+                {
+                    string file = listView1.SelectedItems[0].SubItems[0].Text.Trim();
+                    Client client = new Client(Host, UserName, Password);
+                    if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                        return;
+                    string downloadFile = saveFileDialog1.FileName;
+                    client.DownloadFile(file, downloadFile);
+                    btnConnect_Click(null, null);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString() + ": \n" + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите файл");
+            }
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count <= 0)
+            {
+                MessageBox.Show("Для удаления необходимо выделить файл или директорию, которую желаете удалить");
+                return;
+            }
+            string ftype = listView1.SelectedItems[0].SubItems[1].Text.Trim();
+            if (ftype != "<DIR>")
+            {
+                try
+                {
+                    string deletefile = listView1.SelectedItems[0].SubItems[0].Text.Trim();
+                    Client client = new Client(Host, UserName, Password);
+                    client.DeleteFile(deletefile);
+                    btnConnect_Click(null, null);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString() + ": \n" + ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    string deletefolder = listView1.SelectedItems[0].SubItems[0].Text.Trim();
+                    Client client = new Client(Host, UserName, Password);
+                    client.RemoveDirectory(deletefolder);
+                    btnConnect_Click(null, null);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString() + ": \n" + ex.Message);
+                }
+            }
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            string dir_name = Interaction.InputBox("Задайте имя папки", "Create Folder");
+            if (dir_name == "")
+            {
+                MessageBox.Show("Имя папки не может быть пустым");
+                return;
+            }
+            Client client = new Client(Host, UserName, Password);
+            client.MakeDirectory(dir_name);
+            btnConnect_Click(null, null);
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            Host = "";
+            first = "";
+            listView1.Items.Clear();
+            btnConnect.Visible = true;
+            toolStrip1.Visible = false;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            if (txtHost.Text == null || txtHost.Text == "")
+            {
+                MessageBox.Show("Имя сервера не может быть пустым");
+                return;
+            }
+            if (Host == "")
+            {
+                Host = txtHost.Text;
+                first = Host;
+            }
+            UserName = txtUsername.Text;
+            Password = txtPassword.Text;
+
+            try
+            {
+                Client client = new Client(Host, UserName, Password);
+                Regex regex = new Regex(@"^([d-])([rwxt-]{3}){3}\s+\d{1,}\s+.*?(\d{1,})\s+(\w+\s+\d{1,2}\s+(?:\d{4})?)(\d{1,2}:\d{2})?\s+(.+?)\s?$",
+                    RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+
+                List<FileDirectoryInfo> list = client.ListDirectoryDetails()
+                                                    .Select(s =>
+                                                    {
+                                                        Match match = regex.Match(s);
+                                                        if (match.Length > 5)
+                                                        {                                                          
+                                                            string type = match.Groups[1].Value == "d" ? "DIR.png" : "FILE.png";
+                                                            string size = "";
+                                                            if (long.Parse(match.Groups[3].Value) == 4096)
+                                                            {
+                                                                size = "<DIR>";
+                                                            }
+                                                            else
+                                                            {
+                                                                size = $"{long.Parse(match.Groups[3].Value) / 1024} кБ";
+                                                            }
+                                                            return new FileDirectoryInfo(size, type, match.Groups[6].Value, match.Groups[4].Value, txtHost.Text);
+                                                        }
+                                                        else return new FileDirectoryInfo();
+                                                    }).ToList();
+
+                foreach (var item in list)
+                {
+                    ListViewItem lvi = new ListViewItem();
+                    if (item.Type == "DIR.png")
+                    {
+                        lvi.ImageIndex = 0;
+                    }
+                    else
+                    {
+                        lvi.ImageIndex = 1;
+                    }
+                    lvi.Text = item.Name;
+                    lvi.SubItems.Add(item.FileSize);
+                    lvi.SubItems.Add(item.Date);
+                    if (item.Name != "" && item.Name != "." && item.Name != "..")
+                    {
+                        listView1.Items.Add(lvi);
+                    }
+                }
+                btnConnect.Visible = false;
+                toolStrip1.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString() + ": \n" + ex.Message);
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView1.SelectedItems.Count <= 0)
+            {
+                MessageBox.Show("Выделите папку для просмотра");
+                return;
+            }
+            string ftype = listView1.SelectedItems[0].SubItems[1].Text.Trim();
+            if (ftype == "<DIR>")
+            {
+                try
+                {
+                    string directory = listView1.SelectedItems[0].SubItems[0].Text.Trim();
+                    Client client = new Client(Host, UserName, Password);
+                    client.ChangeWorkingDirectory(directory);
+                    Host = client.ReturnUri();
+                    btnConnect_Click(null, null);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString() + ": \n" + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Для просмотра файла необходимо его скачать");
+            }
+
+        }
+    }
+}
